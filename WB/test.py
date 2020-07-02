f250133b86a4a3c71b9149b4fc7ac070983deeeb
PratikLomte <pratiklomte43@gmail.com>
2020-07-03 00:00:21 +0530

    WEbsite

---
+++ WB/test.py
@@ -0,0 +1,5 @@
+def fun(data):
+	with open("database.txt", mode = 'a') as database:
+		email = data
+		file = database.write(a)
+
---
+++ WB/tp.py
@@ -0,0 +1,5 @@
+def fun(word):
+	a = '/' + word + '.html'
+	print(a)
+	return None
+fun('lol')
